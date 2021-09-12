dff85e5e60f47b452e837277afd791eee62862ea
incredibledevelopers <56466746+incredibledevelopers@users.noreply.github.com>
2021-09-12 20:06:41 +0530

    Angular code added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/dist'));
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5001));
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
