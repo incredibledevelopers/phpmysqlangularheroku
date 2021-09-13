d8b895a4a37423a2db0ffc8033dbc559cfb03fdc
incredibledevelopers <56466746+incredibledevelopers@users.noreply.github.com>
2021-09-13 10:45:34 +0530

    Angular code added

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-
 const express = require('express');
 const app = express();
-
-app.use(requireHTTPS);
-app.use(express.static('./dist/angular-heroku'));
-
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
-);
-
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/dist'));
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
